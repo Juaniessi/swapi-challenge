b90e188c7246d4b729188a4221c4a45106c4a7ae
Juaniessi <juaniessi@gmail.com>
2022-11-09 21:24:44 -0300

    folder name properly fixed

--- pages/character-page.jsx
+++ pages/character-page.jsx
@@ -1,7 +1,7 @@
 import styles from '../styles/Home.module.css';
 import {useState} from 'react';
 import {useRouter} from 'next/router';
-import Button from '../components/boton';
+import Button from '../components/Button';
 
 export default function Character(props) {
 	console.log(props);
